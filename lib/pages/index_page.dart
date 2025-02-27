4facdf13ad1fdf94326059e0ef5cffdcb28be33f
LyNNxMooon <kyawlinthant128380@gmail.com>
2025-02-28 01:19:46 +1100

    video player

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -57,7 +57,7 @@ class _IndexPageState extends State<IndexPage> {
                               ? FAQPage()
                               : SizedBox(),
           Center(child: Text("Copyright 2025 - AI-Solution")),
-          const Gap(20)
+          const Gap(30)
         ],
       ),
     );
